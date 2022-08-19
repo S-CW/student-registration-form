2949a948d91bc2e73c5d47fa34e445eae1595549
DESKTOP-B13A0J4\TTWSHARK <jaysoncheng999@gmail.com>
2022-08-19 16:25:59 +0800

    no message

--- website/settings.py
+++ website/settings.py
@@ -4,8 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 #environment variables
 
+
 # Add configuration to factory application.
-SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI').replace("postgres://", "postgresql://", 1)
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # Athentication setting
