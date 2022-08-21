b42dba04f37ed51b7edfac144a3845d3a4d16ece
DESKTOP-B13A0J4\TTWSHARK <jaysoncheng999@gmail.com>
2022-08-22 01:51:30 +0800

    Validate existing user

--- website/forms.py
+++ website/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField
+from wtforms import StringField, PasswordField, BooleanField, ValidationError
 from wtforms_sqlalchemy.fields import QuerySelectField
 from wtforms.validators import InputRequired, Email, Length
 
-from models import Course, Session
+from models import Course, Session, Student
 
 class Login(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
@@ -15,6 +15,15 @@ class RegistrationForm(FlaskForm):
     student_name = StringField('name', validators=[InputRequired()])
     email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+
+    def validate_email(self, email):
+        if Student.query.filter_by(email=email.data).first():
+            raise ValidationError("Email already registered!")
+
+    def validate_username(self, username):
+        if Student.query.filter_by(username=username.data).first():
+            raise ValidationError("Username already taken! Please try a different username.")
+
 
 def choice_query_option():
     return Course.query
